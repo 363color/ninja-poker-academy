0f7d6eeae35bec2000feb1cb7d3304e02b82f48d
Joel <363color@gmail.com>
2026-06-13 14:35:21 -0400

    feat: instalar Next.js 14 + Payload CMS 3.0 (monorepo)

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260613_154543 from './20260613_154543';
+
+export const migrations = [
+  {
+    up: migration_20260613_154543.up,
+    down: migration_20260613_154543.down,
+    name: '20260613_154543'
+  },
+];
